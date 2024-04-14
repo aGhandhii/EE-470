ad30093a8e5af5ad96e5e60731a35d1656c15d62
jettstreet <jettstreet@gmail.com>
2024-04-14 15:05:07 -0700

    export to mem file

--- test/midibench.py
+++ test/midibench.py
@@ -1,4 +1,5 @@
 from mido import Message, MidiFile, MidiTrack, MetaMessage, tick2second
+import pandas as pd
 import math
 
 mid = MidiFile("Smboverworld.mid")
@@ -23,7 +24,21 @@ def ticks_to_cycles(ticks, ticks_per_beat, tempo, clock_T):
     cycles = seconds / clock_T
     return int(cycles)
 
+def pulse_freq_to_period(freq):
+    return int(2048 - (131072 / freq))
+
+def custom_freq_to_period(freq):
+    return  int(2048 - (65536 / freq))
+
+def velocity_to_volume(velocity):
+    return int((velocity / 127) * 15)
+
 tick_T = ticks_to_cycles(1, ticks_per_beat, tempo, clock_T) # 1 tick in cycles
+
+pulse_1 = pd.DataFrame(columns=['length', 'volume', 'period'])
+pulse_2 = pd.DataFrame(columns=['length', 'volume', 'period'])
+custom = pd.DataFrame(columns=['length', 'volume', 'period'])
+noise = pd.DataFrame(columns=['length', 'volume'])
 
 midi_data = []
 
@@ -37,4 +52,36 @@ for i, track in enumerate(mid.tracks): # parse midi data to dict
                 'cycles': (tick_T * msg.time)
             })
 
-# print(midi_data)
+for data in midi_data:
+    if 'track' in data:
+        new_row = pd.DataFrame({
+            'length': [data['cycles']],
+            'volume': [velocity_to_volume(data['velocity'])],
+            'period': [pulse_freq_to_period(data['freq'])] if data['track'] in [1, 2, 3] else None
+        })
+        if data['track'] == 1:
+            pulse_1 = pd.concat([pulse_1, new_row], ignore_index=True)
+        elif data['track'] == 2:
+            pulse_2 = pd.concat([pulse_2, new_row], ignore_index=True)
+        elif data['track'] == 3:
+            custom = pd.concat([custom, new_row], ignore_index=True)
+        elif data['track'] == 4:
+            noise = pd.concat([noise, new_row.drop(columns='period')], ignore_index=True)
+
+# append 00 to length in custom channel
+
+def export_to_mem_file(df, filename, length_bits):
+    with open(filename, 'w') as f:
+        for _, row in df.iterrows():
+            length = format(int(row['length']), f'0{length_bits}b')
+            volume = format(int(row['volume']), '04b')
+            line = f"{length} {volume}"
+            if 'period' in row and not pd.isna(row['period']):
+                period = format(int(row['period']), '011b')
+                line += f" {period}"
+            f.write(f"{line}\n")
+
+export_to_mem_file(pulse_1, 'pulse_1.mem', 6)
+export_to_mem_file(pulse_2, 'pulse_2.mem', 6)
+export_to_mem_file(custom, 'custom.mem', 8)
+export_to_mem_file(noise, 'noise.mem', 6)
